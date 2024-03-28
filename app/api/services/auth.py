cc7b5e4f51dfe9fbc44e326472b52468d2e62649
Your Name <you@example.com>
2024-03-28 13:22:11 +0300

    added users services

---
+++ app/api/services/auth.py
@@ -0,0 +1,30 @@
+from fastapi_users import FastAPIUsers
+from fastapi_users.authentication import CookieTransport, AuthenticationBackend
+from fastapi_users.authentication import JWTStrategy
+
+from app.api.services.manager import get_user_manager
+from app.core.settings.app_settings import settings
+from app.db import User
+
+cookie_transport = CookieTransport(cookie_name="articles", cookie_max_age=3600)
+
+SECRET = settings.secret_key
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+current_active_user = fastapi_users.current_user(active=True)
---
+++ app/api/services/users.py
@@ -0,0 +1,4 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.schemas.users import UserCreate
+
