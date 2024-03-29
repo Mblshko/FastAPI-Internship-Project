6b5c720bf9c5e122d064ba55c91de9d19c51c423
Your Name <you@example.com>
2024-03-29 15:12:58 +0300

    added show comments

--- app/api/routers/articles.py
+++ app/api/routers/articles.py
@@ -4,8 +4,10 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.services.auth import current_active_user
+from app.api.services.comments import CommentsService
 from app.db import User
-from app.db.schemas.articles import Article, ArticleCreate
+from app.db.schemas.articles import Article, ArticleCreate, ArticleDetail
+from app.db.schemas.comments import Comment, CommentCreate
 from app.db.base import get_db
 from app.api.services.articles import ArticlesCRUD
 
@@ -18,7 +20,7 @@ async def get_articles(session: AsyncSession = Depends(get_db)):
     return await ArticlesCRUD.list_article(session=session)
 
 
-@router.get("/{article_id}", response_model=Article, status_code=200)
+@router.get("/{article_id}", response_model=ArticleDetail, status_code=200)
 async def get_article(article_id: int, session: AsyncSession = Depends(get_db)):
     return await ArticlesCRUD.detail_article(session=session, article_id=article_id)
 
@@ -47,4 +49,16 @@ async def update_article(
 ):
     return await ArticlesCRUD.update_article(
         article_id=article_id, article_data=article_data, session=session
+    )
+
+
+@router.post("/{article_id}/comment", response_model=Comment)
+async def create_comment(
+    article_id: int,
+    comment_data: Annotated[CommentCreate, Depends()],
+    session: AsyncSession = Depends(get_db),
+    user: User = Depends(current_active_user),
+):
+    return await CommentsService.create_comment(
+        article_id=article_id, comment_data=comment_data, session=session, user=user
     )
