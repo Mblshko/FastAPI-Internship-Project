77a5940e7409ef53c9947d722a2d146448824881
Your Name <you@example.com>
2024-03-29 15:14:39 +0300

    refactored

--- app/db/schemas/users.py
+++ app/db/schemas/users.py
@@ -4,15 +4,6 @@ from fastapi_users import schemas
 from pydantic import EmailStr
 
 
-class UserRead(schemas.BaseUser[int]):
-    id: int
-    username: str
-    email: EmailStr
-    is_active: bool = True
-    is_superuser: bool = False
-    is_verified: bool = False
-
-
 class UserCreate(schemas.BaseUserCreate):
     username: str
     email: EmailStr
@@ -20,3 +11,14 @@ class UserCreate(schemas.BaseUserCreate):
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
     is_verified: Optional[bool] = False
+
+
+class UserRead(schemas.BaseUser[int]):
+    """Response"""
+
+    id: int
+    username: str
+    email: EmailStr
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
