4b9980294b5fe053e6819b4527c532c6a219fadd
Your Name <you@example.com>
2024-03-29 15:52:19 +0300

    added new routers

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 
 from app.api.routers.articles import router as articles_router
 from app.api.routers.comments import router as comments_router
+from app.api.routers.profile import router as profile_router
 from app.api.services.auth import auth_backend, fastapi_users, current_active_user
 from app.db import User
 from app.db.schemas.users import UserRead, UserCreate
@@ -11,24 +12,25 @@ app = FastAPI()
 app.include_router(
     router=articles_router,
     prefix="/articles",
-    tags=["articles"],
+    tags=["Articles"],
 )
 
-app.include_router(router=comments_router, prefix="/comment", tags=["comments"])
+app.include_router(router=comments_router, prefix="/comments", tags=["Comments"])
+app.include_router(router=profile_router, prefix="/profile", tags=["Profile"])
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth/jwt",
-    tags=["auth"],
+    tags=["Auth"],
 )
 
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
-    tags=["auth"],
+    tags=["Auth"],
 )
 
 
 @app.get("/authenticated-route")
 async def authenticated_route(user: User = Depends(current_active_user)):
-    return {"message": f"Hello {user.email}!"}
+    return {"message": f"Hello {user.username}!"}
