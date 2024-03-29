883f0ab5e45f576cfa197650135f5fdadef265a6
Your Name <you@example.com>
2024-03-29 17:04:13 +0300

    new middelware

---
+++ app/core/middelware.py
@@ -0,0 +1,10 @@
+from fastapi import Request
+
+
+class CheckActiveUser:
+
+    async def __call__(self, request: Request, call_next):
+        print(f"Request: {request.base_url.hostname}{request.url.path}")
+        response = await call_next(request)
+        print(f"Finish: {request.base_url.hostname}{request.url.path}")
+        return response
