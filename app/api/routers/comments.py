e0860cc6ad9e5ff3aa8c8c48baca56c198cc6add
Your Name <you@example.com>
2024-03-29 15:22:11 +0300

    added create comment

---
+++ app/api/routers/comments.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api.services.auth import current_active_user
+from app.api.services.comments import CommentsService
+from app.db import User
+from app.db.base import get_db
+from app.db.schemas.comments import Comment, CommentCreate
+
+router = APIRouter()
+
+
+@router.post("/{article_id}", response_model=Comment)
+async def create_comment(
+    article_id: int,
+    comment_data: Annotated[CommentCreate, Depends()],
+    session: AsyncSession = Depends(get_db),
+    user: User = Depends(current_active_user),
+):
+    return await CommentsService.create_comment(
+        article_id=article_id, comment_data=comment_data, session=session, user=user
+    )
